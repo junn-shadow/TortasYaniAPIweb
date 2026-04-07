28817645101d76d727381e46160f9110554e6d8e
Junnior Marocho <junniormarocho4@gmail.com>
2026-04-06 23:43:00 -0500

    Limpiador de esquema para reiniciar base de datos por EF

--- Program.cs
+++ Program.cs
@@ -73,6 +73,11 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // Hotfix temporal: Elimina el esquema corrupto/viejo
+    try {
+        db.Database.ExecuteSqlRaw("DROP TABLE IF EXISTS \"Users\" CASCADE; DROP TABLE IF EXISTS \"__EFMigrationsHistory\" CASCADE;");
+    } catch {}
+    
     db.Database.Migrate();
 }
 
