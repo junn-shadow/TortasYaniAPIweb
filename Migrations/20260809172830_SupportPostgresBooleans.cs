1a72f45cc894e450a04865e64589e813fbe8b396
Junnior Marocho <junniormarocho4@gmail.com>
2026-08-09 12:52:22 -0500

    Fix postgres boolean migration

--- Migrations/20260809172830_SupportPostgresBooleans.cs
+++ Migrations/20260809172830_SupportPostgresBooleans.cs
@@ -10,15 +10,22 @@ namespace TortasYaniAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Forzar alteración de columna para que pase de INTEGER a boolean en PostgreSQL
-            migrationBuilder.AlterColumn<bool>(
-                name: "Activo",
-                table: "Users",
-                type: "boolean",
-                nullable: false,
-                defaultValue: true,
-                oldClrType: typeof(int),
-                oldType: "INTEGER");
+            if (migrationBuilder.ActiveProvider == "Npgsql.EntityFrameworkCore.PostgreSQL")
+            {
+                migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"Activo\" TYPE boolean USING CASE WHEN \"Activo\" = 1 THEN true ELSE false END;");
+                migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"Activo\" SET DEFAULT true;");
+            }
+            else
+            {
+                migrationBuilder.AlterColumn<bool>(
+                    name: "Activo",
+                    table: "Users",
+                    type: "INTEGER",
+                    nullable: false,
+                    defaultValue: true,
+                    oldClrType: typeof(int),
+                    oldType: "INTEGER");
+            }
         }
 
         /// <inheritdoc />
